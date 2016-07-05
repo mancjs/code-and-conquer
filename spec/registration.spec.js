7069b44e8f609000c308256559d92c3187a5911f
Martin Rue <hello@martinrue.com>
2016-07-05 22:31:21 +0100

    fix registration tests

--- spec/registration.spec.js
+++ spec/registration.spec.js
@@ -19,7 +19,7 @@ describe('registration', () => {
   it('rejects when registration is closed', () => {
     registration.close();
 
-    const response = registration.createTeam('Team Name', 'user@host.com', 'minelayer');
+    const response = registration.createTeam('Team_Name', 'user@host.com', 'minelayer');
 
     expect(response.err).to.match(/closed/);
     expect(db.get().teams.length).to.be(0);
@@ -55,7 +55,7 @@ describe('registration', () => {
   it('rejects when email is missing', () => {
     registration.open();
 
-    const response = registration.createTeam('Team Name', null, 'minelayer');
+    const response = registration.createTeam('Team_Name', null, 'minelayer');
 
     expect(response.err).to.match(/email address/);
     expect(db.get().teams.length).to.be(0);
@@ -64,7 +64,7 @@ describe('registration', () => {
   it('rejects when email is more than 50 chars', () => {
     registration.open();
 
-    const response = registration.createTeam('Team Name', createString(51), 'cloaker');
+    const response = registration.createTeam('Team_Name', createString(51), 'cloaker');
 
     expect(response.err).to.match(/email address/);
     expect(db.get().teams.length).to.be(0);
@@ -73,7 +73,7 @@ describe('registration', () => {
   it('rejects when role is missing', () => {
     registration.open();
 
-    const response = registration.createTeam('Existing Team Name', 'user@host.com', null);
+    const response = registration.createTeam('Existing_Team_Name', 'user@host.com', null);
 
     expect(response.err).to.match(/Valid roles/);
     expect(db.get().teams.length).to.be(0);
@@ -82,7 +82,7 @@ describe('registration', () => {
   it('rejects when role is invalid', () => {
     registration.open();
 
-    const response = registration.createTeam('Existing Team Name', 'user@host.com', 'invalid');
+    const response = registration.createTeam('Existing_Team_Name', 'user@host.com', 'invalid');
 
     expect(response.err).to.match(/Valid roles/);
     expect(db.get().teams.length).to.be(0);
@@ -92,7 +92,7 @@ describe('registration', () => {
     registration.open();
 
     db.get().teams.push({ email: 'existing@host.com' });
-    const response = registration.createTeam('Team Name', 'existing@host.com', 'minelayer');
+    const response = registration.createTeam('Team_Name', 'existing@host.com', 'minelayer');
 
     expect(response.err).to.match(/same name or email/);
     expect(db.get().teams.length).to.be(1);
@@ -101,8 +101,8 @@ describe('registration', () => {
   it('rejects when name is already in use', () => {
     registration.open();
 
-    db.get().teams.push({ name: 'Existing Team Name' });
-    const response = registration.createTeam('Existing Team Name', 'user@host.com', 'cloaker');
+    db.get().teams.push({ name: 'Existing_Team_Name' });
+    const response = registration.createTeam('Existing_Team_Name', 'user@host.com', 'cloaker');
 
     expect(response.err).to.match(/same name or email/);
     expect(db.get().teams.length).to.be(1);
@@ -111,7 +111,7 @@ describe('registration', () => {
   it('accepts when name, email and role are valid', () => {
     registration.open();
 
-    const response = registration.createTeam('Team Name', 'user@host.com', 'spy');
+    const response = registration.createTeam('Team_Name', 'user@host.com', 'spy');
     const validKeys = ['key', 'gravatar', 'colour', 'roleUsed', 'role', 'name', 'email', 'requests'];
 
     expect(response.err).to.be(undefined);
@@ -122,8 +122,8 @@ describe('registration', () => {
   it('assigns new colours to successive registrations', () => {
     registration.open();
 
-    const response1 = registration.createTeam('Team Name 1', 'user1@host.com', 'spy');
-    const response2 = registration.createTeam('Team Name 2', 'user2@host.com', 'cloaker');
+    const response1 = registration.createTeam('Team_Name_1', 'user1@host.com', 'spy');
+    const response2 = registration.createTeam('Team_Name_2', 'user2@host.com', 'cloaker');
 
     expect(response1.team.colour).to.not.be(response2.team.colour);
   });
@@ -135,7 +135,7 @@ describe('registration', () => {
       db.get().teams.push({ name: 'some-team' });
     });
 
-    const response = registration.createTeam('Team Name', 'user@host.com', 'spy');
+    const response = registration.createTeam('Team_Name', 'user@host.com', 'spy');
 
     expect(response.err).to.match(/is full/);
     expect(db.get().teams.length).to.be(colours.all.length);
@@ -144,7 +144,7 @@ describe('registration', () => {
   it('returns correct team when queried', () => {
     registration.open();
 
-    const response = registration.createTeam('Team Name', 'user@host.com', 'minelayer');
+    const response = registration.createTeam('Team_Name', 'user@host.com', 'minelayer');
     const team = registration.getTeamByKey(response.team.key);
 
     expect(team).not.to.be(undefined);
@@ -152,7 +152,7 @@ describe('registration', () => {
     expect(team.gravatar).to.be.ok();
     expect(team.colour).to.be(colours.all[0]);
     expect(team.role).to.be('minelayer');
-    expect(team.name).to.be('Team Name');
+    expect(team.name).to.be('Team_Name');
     expect(team.email).to.be('user@host.com');
     expect(team.requests).to.be(30);
   });
